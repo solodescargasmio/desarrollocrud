2ab1b3f3edcc50ae61efbd1654ecdc7b1f399105
Juan <solodescargasmio@gmail.com>
2022-02-16 17:21:10 -0300

    Agregada dependencias que hacian falta en visual code y funcionando persistencia jpa que daba errores en intellij

---
+++ src/main/java/com/sofka/crud/models/UsuarioModel.java
@@ -0,0 +1,75 @@
+package com.sofka.crud.models;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true,nullable = false)
+    
+    private Long id;
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+    
+
+    
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return Integer return the prioridad
+     */
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    /**
+     * @param prioridad the prioridad to set
+     */
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+
+}
